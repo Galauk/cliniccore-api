2a42cd93c86721c8bf3b835b14e4ab9d7da34936
Angelo Brito <angelo@hexacore.com.br>
2026-04-27 13:19:10 -0300

    feat: bootstrap ClinicCore backend with Spring Boot, PostgreSQL and health endpoint

---
+++ src/main/java/com/cliniccore/CliniccoreApiApplication.java
@@ -0,0 +1,12 @@
+package com.cliniccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniccoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliniccoreApiApplication.class, args);
+    }
+}
