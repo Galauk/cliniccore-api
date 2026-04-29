9321a5b746b2f89f42b39181785387b386ef2951
Angelo Brito <angelo@hexacore.com.br>
2026-04-28 22:51:24 -0300

    feat: add user REST controller with basic CRUD endpoints

---
+++ src/main/java/com/cliniccore/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.cliniccore.controller;
+
+import com.cliniccore.entity.User;
+import com.cliniccore.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
