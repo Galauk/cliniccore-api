399d781f1e87c0b3fd01033e71ded1c56475cb4d
Angelo Brito <angelo@hexacore.com.br>
2026-05-19 21:05:44 -0300

    feat: implement jwt service for token generation and validation

---
+++ src/main/java/com/cliniccore/security/JwtService.java
@@ -0,0 +1,49 @@
+package com.cliniccore.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY =
+            "cliniccore_super_secret_key_2026";
+
+    private Key getSigningKey() {
+
+        return Keys.hmacShaKeyFor(
+                SECRET_KEY.getBytes()
+        );
+    }
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                )
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        Claims claims = Jwts
+                .parser()
+                .verifyWith((javax.crypto.SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+}
